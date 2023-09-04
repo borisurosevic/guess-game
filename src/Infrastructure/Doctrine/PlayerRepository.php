64ceb1fa7a2c8ced907e278b7f5a58cb5a45089d
Boris <borisurosevic1@gmail.com>
2023-09-04 21:01:42 +0200

    Snimanje player-a

--- src/Infrastructure/Doctrine/PlayerRepository.php
+++ src/Infrastructure/Doctrine/PlayerRepository.php
@@ -2,15 +2,28 @@
 
 namespace Guess\Infrastructure\Doctrine;
 
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\ORMException;
 use Guess\Domain\Player\Player;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\Persistence\ManagerRegistry;
 use Guess\Domain\Player\PlayerRepositoryInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 class PlayerRepository extends ServiceEntityRepository implements PlayerRepositoryInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Player::class);
+    }
+
+    /**
+     * @param Player $player
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function save(Player $player)
+    {
+        $this->_em->persist($player);
+        $this->_em->flush();
     }
 }
